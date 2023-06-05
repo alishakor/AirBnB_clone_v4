d90284e72cf4811eadc8ade2c9d4f5c679b8734f
root <sideeqkolade@gmail.com>
2023-06-05 02:40:13 +0100

    task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+  $('input[type="checkbox"]').change(function() {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    const lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
